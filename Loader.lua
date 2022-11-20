5e26422a9eaa0662be7a4ff46081d519c70db4cf
Hitori <113095167+Role34@users.noreply.github.com>
2022-11-20 18:19:13 +0800

    updated

---
+++ Loader.lua
@@ -0,0 +1,90 @@
+repeat task.wait() until game.GameId ~= 0
+
+if RobloxRage and RobloxRage.Game then
+    RobloxRage.Utilities.UI:Notification({
+        Title = "I Miss The Rage",
+        Description = "Script already executed!",
+        Duration = 5
+    }) return
+end
+
+local PlayerService = game:GetService("Players")
+repeat task.wait() until PlayerService.LocalPlayer
+local LocalPlayer = PlayerService.LocalPlayer
+local QueueOnTeleport = queue_on_teleport or
+(syn and syn.queue_on_teleport)
+local LoadArgs = {...}
+
+local function GetSupportedGame() local Game
+    for Id,Info in pairs(RobloxRage.Games) do
+        if tostring(game.GameId) == Id then
+            Game = Info break
+        end
+    end if not Game then
+        return RobloxRage.Games.Universal
+    end return Game
+end
+
+local function Concat(Array,Separator)
+    local Output = "" for Index,Value in ipairs(Array) do
+        Output = Index == #Array and Output .. tostring(Value)
+        or Output .. tostring(Value) .. Separator
+    end return Output
+end
+
+local function GetScript(Script)
+    return RobloxRage.Debug and readfile("RobloxRage/" .. Script .. ".lua")
+    or game:HttpGetAsync(("%s%s.lua"):format(RobloxRage.Domain,Script))
+end
+
+local function LoadScript(Script)
+    return loadstring(RobloxRage.Debug and readfile("RobloxRage/" .. Script .. ".lua")
+    or game:HttpGetAsync(("%s%s.lua"):format(RobloxRage.Domain,Script)))()
+end
+
+getgenv().RobloxRage = {Debug = LoadArgs[1],Utilities = {},
+    Domain = "https://raw.githubusercontent.com/Role34/RobloxRage/main/",Games = {
+        ["Universal" ] = {Name = "Universal",                       Script = "Universal"   },
+        ["873703865" ] = {Name = "Westbound",                       Script = "Games/wb"    },
+        ["1168263273"] = {Name = "Bad Business",                    Script = "Games/bb"    },
+        ["358276974" ] = {Name = "Apocalypse Rising 2",             Script = "Games/ar2"   },
+        ["1054526971"] = {Name = "Blackhawk Rescue Mission 5",      Script = "Games/brm5"  }
+    }
+}
+
+--[[
+getgenv().RobloxRage = {Domain = "https://raw.githubusercontent.com/Role34/RobloxRage/main",
+    Debug = LoadArgs[1],Game = "None",Loaded = false,Utilities = {},Games = {
+        [""          ] = {Name = "",                                Script = "Games/"      },
+        ["Universal" ] = {Name = "Universal",                       Script = "Universal"   },
+        ["873703865" ] = {Name = "Westbound",                       Script = "Games/wb"    },
+        ["1168263273"] = {Name = "Bad Business",                    Script = "Games/bb"    },
+        ["358276974" ] = {Name = "Apocalypse Rising 2",             Script = "Games/ar2"   },
+        ["1054526971"] = {Name = "Blackhawk Rescue Mission 5",      Script = "Games/brm5"  }
+    }
+}
+]]
+
+RobloxRage.Utilities.UI = LoadScript("Utilities/UI")
+RobloxRage.Utilities.Misc = LoadScript("Utilities/Misc")
+RobloxRage.Utilities.Drawing = LoadScript("Utilities/Drawing")
+
+local SupportedGame = GetSupportedGame()
+LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        QueueOnTeleport(([[local LoadArgs = {%s}
+        loadstring(LoadArgs[1] and readfile("RobloxRage/Loader.lua") or
+        game:HttpGetAsync("%sLoader.lua"))(unpack(LoadArgs))
+        ]]):format(Concat(LoadArgs,","),RobloxRage.Domain))
+    end
+end)
+
+if SupportedGame then
+    RobloxRage.Game = SupportedGame.Name
+    LoadScript(SupportedGame.Script)
+    RobloxRage.Utilities.UI:Notification({
+        Title = "I Miss The Rage",
+        Description = RobloxRage.Game .. " loaded!",
+        Duration = LoadArgs[2]
+    })
+end
---
+++ README.md
@@ -0,0 +1,99 @@
+### Roblox Script Hub designed for shooters.
+***I Miss The Rage is still in beta stage of development so expect some bugs or lack of features.***  
+Modified by ***Role34***
+
+### Loadstring
+```lua
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Role34/RobloxRage/main/Loader.lua"))()
+```
+```load args to loader
+loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Role34/RobloxRage/main/Loader.lua"))(false,5)
+-                                                                                                debug ^   ^ notify time
+```
+### Supported Games
+- [Bad Business](https://www.roblox.com/games/3233893879/)
+- [Blackhawk Rescue Mission 5](https://www.roblox.com/games/2916899287/)
+- [Westbound](https://www.roblox.com/games/2474168535/)
+- [Steel Titans](https://www.roblox.com/games/4746041618/)
+- [Apocalypse Rising 2](https://www.roblox.com/games/863266079/)
+### Features
+```markdown
+# Universal
+  - Aimbot
+    - Visibility Check
+    - Distance Check
+    - Smoothness
+    - Team Check
+    - Prediction
+    - Body Parts (Priority)
+    - FOV Circle Customization
+  - Silent Aim
+    - Visibility Check
+    - Distance Check
+    - Team Check
+    - Hit Chance
+    - Body Parts (Priority)
+    - FOV Circle Customization
+  - Trigger Bot
+    - Features same as Aimbot
+  - Visuals
+    - Distance Check
+    - Ally Color
+    - Enemy Color
+    - Team Check
+    - Team Color
+    - Boxes
+    - Offscreen Arrows
+    - Head Circles
+    - Tracers
+    - Chams (Highlights)
+  - Misc
+    - Custom Mouse
+    - Custom Crosshair
+    - Rejoin
+    - Server Hop
+
+
+# Blackhawk Rescue Mission 5
+  - NPC ESP
+  - Clock Time Changer
+  - Fog Density Changer
+  - No Recoil
+  - Instant Hit (No Bullet Drop)
+  - Unlock Firemodes
+  - Rapid Fire
+  - Walkspeed Changer
+  - Vehicle Modification
+  - Helicopter Modification
+  - No NVG
+  - NVG No Shape
+  - Fake RGE
+
+
+# Westbound
+  - Instant Interact
+
+
+# Bad Business
+  - Autoshoot (Rage bot)
+  - Weapon Customization
+  - Arms Customization
+  - Weapon Modification
+  - Pitch Anti-Aim
+  - QE Lean Anti-Aim
+  - Fly + Noclip
+  - Anti Kick
+
+
+# Steel Titans
+  - Tank Fly (Funny)
+  - ESP Support
+
+
+# Apocalypse Rising 2
+  - Aimbot Support
+  - ESP Support
+  - Recoil Mods
+  - Item ESP
+  - Map ESP
+```
